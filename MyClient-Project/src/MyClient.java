d63604a021a716416637ff64bb439e527d49c353
EJ-Corp <emgdl2002@gmail.com>
2023-03-27 14:26:25 +1100

    Basic connection to server and first written message

--- MyClient-Project/src/MyClient.java
+++ MyClient-Project/src/MyClient.java
@@ -1,5 +1,31 @@
+import java.io.*;
+import java.net.*;
+
 public class MyClient {
     public static void main(String[] args) {
-        
+        try {
+            //Socket to connect to the server
+            Socket s = new Socket("localhost", 50000);
+
+            //Client output (Our Messages)
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+
+            //Write Our messages to the server
+            dout.write(("HELO\n").getBytes());
+            dout.flush();
+
+            //The messages sent by the server
+            BufferedReader dis = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+            //Read the messages from the server and print them on the console
+            String str = (String)dis.readLine();
+            System.out.println("mesage= " + str + "\n");
+
+            
+            dout.close(); //Close out output to the server (our messages)
+            s.close(); //Close the connection to the server
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
