0f7b48dd6cc0d9e4a372bf280f9742641cd84999
EJ-Corp <emgdl2002@gmail.com>
2023-03-28 21:15:04 +1100

    Attempt to get loop working
    Infinite loop atm, cause schd only happens once

---
+++ MyClient-Project/src/MyClient_Multiple.java
@@ -0,0 +1,142 @@
+import java.io.*;
+import java.net.*;
+
+public class MyClient_Multiple {
+    public static void main(String[] args) {
+        try {
+            //Socket to connect to the server
+            Socket s = new Socket("localhost", 50000);
+
+            //Client output (Our Messages)
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+
+            //Write Our messages to the server
+            dout.write(("HELO\n").getBytes());
+            dout.flush();
+
+            //The messages sent by the server
+            BufferedReader dis = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+            //Read the messages from the server and print them on the console
+            String str = (String)dis.readLine();
+            System.out.println("mesage= " + str + "\n");
+
+            String username = System.getProperty("user.name");
+            dout.write(("AUTH " + username +" \n").getBytes());
+            dout.flush();
+
+            str = (String)dis.readLine();
+            System.out.println("mesage= " + str + "\n");
+
+            //END OF HANDSHAKE
+
+            //Store Variables
+            int nRecs;
+            String[] dataSplit;
+            int nRectsIdx = 1;
+
+            String[] serverTypes;
+		    String[] serverIDs;
+            int[] serverCores;
+
+            int largestServerIdx = -1;
+            int largestServerAmount = 0;
+            int largestServerCores = 0;
+
+            boolean foundLargest = false;
+
+            //Inside a loop:
+            while(true) {
+                if(str.equals("NONE")) {
+                    break;
+                }
+                dout.write(("REDY\n").getBytes());
+                dout.flush();
+
+                str = (String)dis.readLine();
+                System.out.println("mesage= " + str + "\n");
+
+                //Store the job Info from the message sent by the server
+                String[] splitJobInfo = str.split(" ");
+
+                //Store the job Name
+                String jobName = splitJobInfo[0];
+                //Store the ID
+                int jobID = Integer.parseInt(splitJobInfo[2]);
+
+                //Get largest server once
+                if(!foundLargest) {
+                    //Gets all the servers
+                    dout.write(("GETS All\n").getBytes());
+                    dout.flush();
+
+                    str = (String)dis.readLine();
+                    System.out.println("mesage= " + str + "\n");
+
+                    dataSplit = str.split(" ");
+
+                    //Store number of servers
+                    nRecs = Integer.parseInt(dataSplit[nRectsIdx]);
+
+                    //recieve all servers info
+                    dout.write(("OK\n").getBytes());
+                    dout.flush();
+
+                    //prepare to store them
+                    serverTypes = new String[nRecs];
+                    serverIDs = new String[nRecs];
+                    serverCores = new int[nRecs];
+
+                    for(int i = 0; i < nRecs; i++) {
+                        str = (String)dis.readLine();
+                        System.out.println("mesage= " + str + "\n");
+
+                        String[] splitServer = str.split(" ");
+                        serverTypes[i] = splitServer[0];
+                        serverIDs[i] = splitServer[1];
+                        serverCores[i] = Integer.parseInt(splitServer[4]);
+
+                        if(serverCores[i] >= largestServerCores) {
+                            largestServerIdx = i;
+                            largestServerCores = serverCores[i];
+                        }
+                    }
+
+                    for(int i = 0; i < serverCores.length; i++) {
+                        if(serverCores[i] == largestServerCores) {
+                            largestServerAmount++;
+                        }
+                    }
+
+                    foundLargest = true;
+                    System.out.println("Largest Server: " + serverTypes[largestServerIdx] + " Cores: " + largestServerCores + " Amount of Servers: " + largestServerAmount);
+                    
+                    dout.write(("OK\n").getBytes());
+                    dout.flush();
+
+                    str = (String)dis.readLine();
+                    System.out.println("mesage= " + str + "\n");
+
+                    if(jobName.equals("JOBN")) {
+                        dout.write(("SCHD " + jobID + " " + serverTypes[largestServerIdx] + " " + serverIDs[largestServerIdx] + " \n").getBytes());
+                        dout.flush();
+        
+                        str = (String)dis.readLine();
+                        System.out.println("mesage= " + str + "\n");
+                    }
+                }
+            }                 
+
+            dout.write(("QUIT\n").getBytes());
+            dout.flush();
+
+            str = (String)dis.readLine();
+            System.out.println("mesage= " + str + "\n");
+
+            dout.close(); //Close out output to the server (our messages)
+            s.close(); //Close the connection to the server
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
