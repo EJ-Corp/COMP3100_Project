a6059bae47c88c11ed5136762e01f60a473a0d82
EJ-Corp <emgdl2002@gmail.com>
2023-05-25 16:17:58 +1000

    Store the first job + details needed

--- MyClient-Project/src/MyClient_Stage2.java
+++ MyClient-Project/src/MyClient_Stage2.java
@@ -31,6 +31,32 @@ public class MyClient_Stage2 {
             serverOutput = (String)serverMessages.readLine();
             System.out.println("mesage= " + serverOutput + "\n");
 
+            //Send "REDY" to the server to recieve the first job
+            clientOutput.write(("REDY\n").getBytes());
+            clientOutput.flush();
+
+            //Read the message ("JOBN...subTime ... ID ... estRunTime ... Core ... Mem ... Disk") from the server and print them on the console
+            String job = (String)serverMessages.readLine();
+            System.out.println("mesage= " + serverOutput + "\n");
+
+            //Store the job Info from the message sent by the server
+            String[] splitJobInfo = job.split(" ");
+            //Store the job Name
+            String jobName = splitJobInfo[0];
+            //Store the ID
+            int jobID = Integer.parseInt(splitJobInfo[2]);
+            //Store the Cores
+            int jobCores = Integer.parseInt(splitJobInfo[4]);
+            //Store the Memory
+            int jobMem = Integer.parseInt(splitJobInfo[5]);
+            //Store the Disk
+            int jobDisk = Integer.parseInt(splitJobInfo[6]);
+
+            //Ask for capable server to run the job
+            clientOutput.write(("GETS Capable " + jobCores + " " + jobMem + " " + jobDisk+ "\n").getBytes());
+            clientOutput.flush();
+
+
 
         } catch (Exception e) {
             System.out.println(e);
